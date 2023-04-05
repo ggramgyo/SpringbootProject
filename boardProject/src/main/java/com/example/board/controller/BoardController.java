8b1327efff6074664f1794aefb4d9e97e2b40c16
ggramgyo <woghk6761@pusan.ac.kr>
2023-04-06 01:31:06 +0900

    feature: 게시글 상세 조회 기능 추가

--- boardProject/src/main/java/com/example/board/controller/BoardController.java
+++ boardProject/src/main/java/com/example/board/controller/BoardController.java
@@ -5,10 +5,7 @@ import com.example.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,5 +36,18 @@ public class BoardController {
         model.addAttribute("boardList", boardDTOList);
 
         return "list";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id, Model model){
+        /*
+            해당 게시글의 조회수를 하나 올리고
+            게시글 데이터를 가져와서 detail.html 출력
+         */
+        boardService.updateHits(id);
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        return "detail";
+
     }
 }
