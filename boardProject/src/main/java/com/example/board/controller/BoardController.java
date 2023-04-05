59b6c31f0e22921c45adcfa0d6cf1b0cfd10a8ef
ggramgyo <woghk6761@pusan.ac.kr>
2023-04-06 01:09:02 +0900

    feature: 게시글 전체 목록 조회 기능 추가

--- boardProject/src/main/java/com/example/board/controller/BoardController.java
+++ boardProject/src/main/java/com/example/board/controller/BoardController.java
@@ -4,10 +4,13 @@ import com.example.board.dto.BoardDTO;
 import com.example.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,5 +30,14 @@ public class BoardController {
         System.out.println("boardDTO = " + boardDTO);
         boardService.save(boardDTO);
         return "index";
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model){
+        // db 에서 전체 게시글 데이터를 가져와서 list.html 로 전송해줌
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+
+        return "list";
     }
 }
--- boardProject/src/main/java/com/example/board/dto/BoardDTO.java
+++ boardProject/src/main/java/com/example/board/dto/BoardDTO.java
@@ -1,5 +1,6 @@
 package com.example.board.dto;
 
+import com.example.board.entity.BoardEntity;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -16,7 +17,24 @@ public class BoardDTO {
     private String boardPass;
     private String boardTitle;
     private String boardContents;
+    private int boardHits;
     private LocalDateTime boardCreatedTime;
     private LocalDateTime boardUpdatedTime;
+
+
+    public static BoardDTO toBoardDTO(BoardEntity boardEntity) {
+        BoardDTO boardDTO = new BoardDTO();
+        boardDTO.setId(boardEntity.getId());
+        boardDTO.setBoardWriter(boardEntity.getBoardWriter());
+        boardDTO.setBoardPass(boardEntity.getBoardPass());
+        boardDTO.setBoardTitle(boardEntity.getBoardTitle());
+        boardDTO.setBoardContents(boardEntity.getBoardContents());
+        boardDTO.setBoardHits(boardEntity.getBoardHits());
+        boardDTO.setBoardCreatedTime(boardEntity.getCreatedTime());
+        boardDTO.setBoardUpdatedTime(boardEntity.getUpdatedTime());
+
+
+        return boardDTO;
+    }
 
 }
